22b4a4524da322b2c4be493cf2d3eaeaee873841
Chijioke Ugwuanyi <cj.ugwuanyi@gmail.com>
2022-04-01 09:15:23 +0200

    Initialize Gradle project with Kotlin DSL

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.2.5" apply false
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.aegis"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+dependencies {
+    // Spring Boot dependencies will be added in subsequent tasks
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
+}
