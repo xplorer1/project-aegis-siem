497005798e5f6d7e279f2e76626dd57f433373ae
Chijioke Ugwuanyi <cj.ugwuanyi@gmail.com>
2022-04-16 14:12:35 +0200

    Add core fields to OcsfEvent

--- src/main/java/com/aegis/domain/OcsfEvent.java
+++ src/main/java/com/aegis/domain/OcsfEvent.java
@@ -3,6 +3,10 @@ package com.aegis.domain;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import java.time.Instant;
 
 /**
  * Base class for OCSF (Open Cybersecurity Schema Framework) events.
@@ -14,6 +18,26 @@ public class OcsfEvent {
     @Id
     @JsonProperty("uuid")
     private String uuid;
+    
+    @Field(type = FieldType.Date)
+    @JsonProperty("time")
+    private Instant time;
+    
+    @Field(type = FieldType.Keyword)
+    @JsonProperty("tenant_id")
+    private String tenantId;
+    
+    @Field(type = FieldType.Byte)
+    @JsonProperty("severity")
+    private int severity; // 1=Info, 2=Low, 3=Medium, 4=High, 5=Critical
+    
+    @Field(type = FieldType.Short)
+    @JsonProperty("class_uid")
+    private int classUid; // OCSF class identifier
+    
+    @Field(type = FieldType.Byte)
+    @JsonProperty("category_uid")
+    private int categoryUid; // OCSF category identifier
     
     /**
      * Default constructor for Jackson deserialization
@@ -36,5 +60,45 @@ public class OcsfEvent {
     
     public void setUuid(String uuid) {
         this.uuid = uuid;
+    }
+    
+    public Instant getTime() {
+        return time;
+    }
+    
+    public void setTime(Instant time) {
+        this.time = time;
+    }
+    
+    public String getTenantId() {
+        return tenantId;
+    }
+    
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+    
+    public int getSeverity() {
+        return severity;
+    }
+    
+    public void setSeverity(int severity) {
+        this.severity = severity;
+    }
+    
+    public int getClassUid() {
+        return classUid;
+    }
+    
+    public void setClassUid(int classUid) {
+        this.classUid = classUid;
+    }
+    
+    public int getCategoryUid() {
+        return categoryUid;
+    }
+    
+    public void setCategoryUid(int categoryUid) {
+        this.categoryUid = categoryUid;
     }
 }
