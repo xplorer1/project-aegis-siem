fc60a27ffb95c6446effe5d841550d301eb4a6a7
Chijioke Ugwuanyi <cj.ugwuanyi@gmail.com>
2022-05-09 10:21:22 +0200

    Create KafkaProducerConfig class

---
+++ src/main/java/com/aegis/kafka/KafkaProducerConfig.java
@@ -0,0 +1,52 @@
+package com.aegis.kafka;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Kafka Producer Configuration for AEGIS SIEM
+ * Configures high-throughput, reliable event streaming to Kafka topics
+ */
+@Configuration
+public class KafkaProducerConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    /**
+     * Creates a ProducerFactory with optimized settings for high-throughput event streaming
+     * 
+     * @return ProducerFactory configured for reliable, high-performance event production
+     */
+    @Bean
+    public ProducerFactory<String, Object> producerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        
+        // Basic connection settings
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    /**
+     * Creates a KafkaTemplate for sending messages to Kafka topics
+     * 
+     * @return KafkaTemplate configured with the producer factory
+     */
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
