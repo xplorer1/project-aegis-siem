d3f56947202722edb7c5442e43834a2a5f8ad1b0
Chijioke Ugwuanyi <cj.ugwuanyi@gmail.com>
2022-05-12 09:01:04 +0200

    Add producer metrics

--- src/main/java/com/aegis/kafka/KafkaProducerService.java
+++ src/main/java/com/aegis/kafka/KafkaProducerService.java
@@ -1,5 +1,8 @@
 package com.aegis.kafka;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -18,9 +21,25 @@ public class KafkaProducerService {
     private static final Logger log = LoggerFactory.getLogger(KafkaProducerService.class);
 
     private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final Counter sendSuccessCounter;
+    private final Counter sendFailureCounter;
+    private final Timer sendLatencyTimer;
 
-    public KafkaProducerService(KafkaTemplate<String, Object> kafkaTemplate) {
+    public KafkaProducerService(KafkaTemplate<String, Object> kafkaTemplate, MeterRegistry meterRegistry) {
         this.kafkaTemplate = kafkaTemplate;
+        
+        // Initialize metrics
+        this.sendSuccessCounter = Counter.builder("kafka.producer.send.success")
+            .description("Number of successfully sent messages")
+            .register(meterRegistry);
+        
+        this.sendFailureCounter = Counter.builder("kafka.producer.send.failure")
+            .description("Number of failed message sends")
+            .register(meterRegistry);
+        
+        this.sendLatencyTimer = Timer.builder("kafka.producer.send.latency")
+            .description("Latency of message sends")
+            .register(meterRegistry);
     }
 
     /**
@@ -32,12 +51,18 @@ public class KafkaProducerService {
      * @return CompletableFuture with the send result
      */
     public CompletableFuture<SendResult<String, Object>> send(String topic, String key, Object value) {
+        Timer.Sample sample = Timer.start();
+        
         CompletableFuture<SendResult<String, Object>> future = kafkaTemplate.send(topic, key, value);
         
         future.whenComplete((result, ex) -> {
+            sample.stop(sendLatencyTimer);
+            
             if (ex != null) {
+                sendFailureCounter.increment();
                 log.error("Failed to send message to topic {}: {}", topic, ex.getMessage(), ex);
             } else {
+                sendSuccessCounter.increment();
                 log.debug("Successfully sent message to topic {} partition {} offset {}",
                     topic,
                     result.getRecordMetadata().partition(),
